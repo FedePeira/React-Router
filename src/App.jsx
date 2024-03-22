71719c35d3f970237c55d35155572d6fdd24ce80
Federico Peirano <fedepr2345@gmail.com>
2024-03-22 12:15:32 -0300

    React-Bootstrap - Navbar - Part7.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,39 @@
 import {
-  Routes, Route, Link, Navigate, useMatch
+Link
 } from "react-router-dom"
-import Notes from "./components/Notes"
-import Note from "./components/Note"
-import Login from "./components/Login"
-import Home from "./components/Home"
-import Users from "./components/Users"
 import { useState } from "react"
-import { Alert } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
 const App = () => {
   const [user, setUser] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      content: 'HTML is easy',
-      important: true,
-      user: 'Matti Luukkainen'
-    },
-    {
-      id: 2,
-      content: 'Browser can execute only JavaScript',
-      important: false,
-      user: 'Matti Luukkainen'
-    },
-    {
-      id: 3,
-      content: 'Most important methods of HTTP-protocol are GET and POST',
-      important: true,
-      user: 'Arto Hellas'
-    }
-  ])
-
-  const match = useMatch('/notes/:id')
-  const note = match 
-    ? notes.find(note => note.id === Number(match.params.id))
-    : null
 
   const padding = {
     padding: 5
   }
 
-  const login = (user) => {
-    setUser(user)
-    setMessage(`welcome ${user}`)
-    setTimeout(() => {
-      setMessage(null)
-    }, 10000)
-  }
-
   return (
-    
-    <div className="container">
-      {(message &&
-        <Alert variant="success">
-          {message}
-        </Alert>
-      )}
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/notes">notes</Link>
-        <Link style={padding} to="/users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
-        }
-      </div>
-
-      <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />   
-        <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
-        <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-      <div>
-        <i>Note app, Department of Computer Science 2023</i>
-      </div>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to="/">home</Link>
+          </Nav.Link>
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to="/notes">notes</Link>
+          </Nav.Link>
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to="/users">users</Link>
+          </Nav.Link>
+          <Nav.Link href="#" as="span">
+            {user
+              ? <em>{user} logged in</em>
+              : <Link to="/login">login</Link>
+            }
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
